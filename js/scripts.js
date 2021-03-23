158217beb2eeacc6ca6ca3bf86f3fd48a86fb3e2
Diogo Eugenio da Silva Cortez <1330507@ifrn.local>
2021-03-23 18:02:02 -0300

    Início da implementação da visão computacional.

--- js/scripts.js
+++ js/scripts.js
@@ -43,7 +43,7 @@
     }
   }
 
- function movJogadorK()   //Configurações das teclas do teclado habilitadas para o jogo 
+ function movJogadorK()   //Controle do jogo pelo teclado 
   { 
     document.querySelector('body').addEventListener('keydown', function(event)
     {
@@ -59,7 +59,7 @@
     })
   }
 
-  function movJogadorM()   //Configurações do mouse habilitadas para o jogo 
+  function movJogadorM()   //Controle do jogo pelo mouse 
 {
   document.querySelector('#divAnimacao').addEventListener('mousemove', function(e) //Movimenta jogador
   {
@@ -74,6 +74,66 @@
       $('.j1').css({"top": mY})
     }
   })
+  }
+
+  function movJogadorC()   //Controle do jogo por visão computacional 
+  {
+    // More API functions here:
+    // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
+
+    // the link to your model provided by Teachable Machine export panel
+    const URL = "../js/";
+
+    let model, webcam, labelContainer, maxPredictions;
+
+    // Load the image model and setup the webcam
+    async function init() 
+    {
+      const modelURL = URL + "model.json";
+      const metadataURL = URL + "metadata.json";
+
+      // load the model and metadata
+      // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+      // or files from your local hard drive
+      // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+      model = await tmImage.load(modelURL, metadataURL);
+      maxPredictions = model.getTotalClasses();
+
+      // Convenience function to setup a webcam
+      const flip = true; // whether to flip the webcam
+      webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+      await webcam.setup(); // request access to the webcam
+      await webcam.play();
+      window.requestAnimationFrame(loop);
+
+      // append elements to the DOM
+      document.getElementById("webcam-container").appendChild(webcam.canvas);
+      labelContainer = document.getElementById("label-container");
+      for (let i = 0; i < maxPredictions; i++) 
+      { // and class labels
+          labelContainer.appendChild(document.createElement("div"));
+      }
+    }
+
+    async function loop() 
+    {
+      webcam.update(); // update the webcam frame
+      await predict();
+      window.requestAnimationFrame(loop);
+    }
+
+    // run the webcam image through the image model
+    async function predict() 
+    {
+      // predict can take in an image, video or canvas html element
+      const prediction = await model.predict(webcam.canvas);
+      for (let i = 0; i < maxPredictions; i++) 
+      {
+          const classPrediction =
+          prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+          labelContainer.childNodes[i].innerHTML = classPrediction;
+      }
+    }
   }
 
   function comparaListas(_arr1, _arr2) //Compara a lista com as letras coletadas e a lista com a palavra chave
